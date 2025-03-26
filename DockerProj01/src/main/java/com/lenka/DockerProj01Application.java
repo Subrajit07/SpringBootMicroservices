990ebef614b8c777d5892a2ab1a7fd1cf76cdbf4
Subrajit07 <subrajitlenka1234@gmail.com>
2025-03-26 11:38:38 +0530

    Docker project commit

---
+++ DockerProj01/src/main/java/com/lenka/DockerProj01Application.java
@@ -0,0 +1,14 @@
+package com.lenka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerProj01Application.class, args);
+		System.out.println("Welcome to Docker SpringBoot Application");
+	}
+
+}
