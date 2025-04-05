b32392bed3d0ebd935662cf4217820ee99a29e09
Subrajit07 <subrajitlenka1234@gmail.com>
2025-04-05 10:00:23 +0530

    first commit

---
+++ DockerProj05BasicApp/src/main/java/com/docker/DockerProj05BasicAppApplication.java
@@ -0,0 +1,14 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProj05BasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerProj05BasicAppApplication.class, args);
+		System.out.println("Docker Proj05 Application");
+	}
+
+}
